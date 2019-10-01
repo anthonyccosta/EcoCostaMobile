149d498651ce7892de0007dcaa9b4e4d045c13b4
Anthony Costa <ccosta.anthony@outlook.com>
2019-10-01 16:33:28 -0300

    Menu

---
+++ EcoCostaMobile/EcoCostaMobile/Menu.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EcoCostaMobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+            Detail = new NavigationPage(new MainPage());
+        }
+
+        private void Paginaclientes_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Paginaprodutos_Tapped(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new produtos());
+            IsPresented = false; //fechar o menu ao abrir a pagina desejada
+        }
+
+        private void Paginafornecedores_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Paginacategoria_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
